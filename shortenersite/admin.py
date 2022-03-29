50a64c25307c5de75d04633cb3ccf930133828ae
clownkill <alehnovich90@yandex.ru>
2022-03-29 16:44:29 +0300

    added using @admin.register decorator

--- shortenersite/admin.py
+++ shortenersite/admin.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 
 from shortenersite.models import Url
 
-
+@admin.register(Url)
 class UrlAdmin(admin.ModelAdmin):
     lists_display = ('slug', 'full_url', 'creation_date', 'count_clicks')
-
-
-admin.site.register(Url, UrlAdmin)
